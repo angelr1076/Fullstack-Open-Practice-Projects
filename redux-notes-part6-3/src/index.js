ea07dade1addf1b839a9cea51b8af6e5ee0054b3
Angel Rodriguez <angelr1076@gmail.com>
2021-12-20 07:29:55 -0500

    created store file in redux notes

--- redux-notes-part6-3/src/index.js
+++ redux-notes-part6-3/src/index.js
@@ -1,23 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore, combineReducers } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import { Provider } from 'react-redux' 
-import App from './App'
-
-import noteReducer from './reducers/noteReducer'
-import filterReducer from './reducers/filterReducer'
-
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer
-})
-
-const store = createStore(reducer, composeWithDevTools())
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import App from './App';
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
-)
+);
