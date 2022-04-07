27717568b204f7e23f6c9b322e69811556937f3b
Angel Rodriguez <angelr1076@gmail.com>
2022-04-07 07:48:28 -0400

    Added router practice

--- Chapter7/react-router/src/App.js
+++ Chapter7/react-router/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link
+} from "react-router-dom"
 
-function App() {
+const App = () => {
+
+  const padding = {
+    padding: 5
+  }
+  
+  const Home = () => {
+    return (
+      <h1>Home</h1>
+    )
+  }
+
+  const Notes = () => {
+    return (
+      <h1>Notes</h1>
+    )
+  }
+
+  const Users = () => {
+    return (
+      <h1>Users</h1>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <Link style={padding} to="/">home</Link>
+        <Link style={padding} to="/notes">notes</Link>
+        <Link style={padding} to="/users">users</Link>
+      </div>
+
+      <Routes>
+        <Route path="/notes" element={<Notes />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+
+      <div>
+        <i>Note app, Department of Computer Science 2022</i>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
